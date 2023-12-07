be4a2a626c2ce16eb5e3fe676eb890aa6d31ba97
JacquelineRJ <73772376+JacquelineRJ@users.noreply.github.com>
2023-12-07 14:37:56 -0800

    Upload Comps JS website fiels

---
+++ buttons.js
@@ -0,0 +1,246 @@
+function showcorrecttext(){
+    var hid = document.getElementsByClassName("correct");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "visible";
+    }
+    var hid = document.getElementsByClassName("wrong");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "hidden";
+    }
+}
+function showincorrecttext(){
+    var hid = document.getElementsByClassName("wrong");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "visible";
+    }
+    var hid = document.getElementsByClassName("correct");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "hidden";
+    }
+}
+
+function showbinaryseven(){
+    var hid = document.getElementsByClassName("binary-image");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "visible";
+        hid[0].style.opacity = "100%";
+    }
+}
+function hidebinaryseven(){
+    var hid = document.getElementsByClassName("binary-image");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "hidden";
+        hid[0].style.opacity = "0%";
+    }
+}
+
+function showcon2(){
+var hid = document.getElementsByClassName("con2");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "visible";
+        hid[0].style.opacity = "100%";
+    }
+var hid = document.getElementsByClassName("con3");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "hidden";
+        hid[0].style.opacity = "0%";
+    }
+}
+function showcon3(){
+    var hid = document.getElementsByClassName("con3");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+    }
+
+function showPool2(){
+    var hid = document.getElementsByClassName("maxPool2");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+     var hid = document.getElementsByClassName("poolMNSITbutton");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.display = "none";
+        }
+     var hid = document.getElementsByClassName("poolbutton");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.display = "block";
+        }
+}
+function hidePool2(){
+    var hid = document.getElementsByClassName("maxPool2");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+        var hid = document.getElementsByClassName("poolMNSITbutton");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.display = "block";
+        }
+     var hid = document.getElementsByClassName("poolbutton");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.display = "none";
+        }
+}
+
+function shownetwork1(){
+    var hid = document.getElementsByClassName("network1");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+        var hid = document.getElementsByClassName("network2");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+        var hid = document.getElementsByClassName("network3");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+}
+function shownetwork2(){
+        var hid = document.getElementsByClassName("network2");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+        var hid = document.getElementsByClassName("network3");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+}
+function shownetwork3(){
+    var hid= document.getElementsByClassName("network3");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "visible";
+        hid[0].style.opacity = "100%";
+    }
+}
+function showcnn2(){
+    var hid = document.getElementsByClassName("cnn2");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+        var hid = document.getElementsByClassName("cnn3");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+        var hid = document.getElementsByClassName("cnn4");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+        var hid = document.getElementsByClassName("cnn5");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+}
+function showcnn3(){
+        var hid = document.getElementsByClassName("cnn3");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+        var hid = document.getElementsByClassName("cnn4");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+        var hid = document.getElementsByClassName("cnn5");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+}
+function showcnn4(){
+
+        var hid = document.getElementsByClassName("cnn4");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+        var hid = document.getElementsByClassName("cnn5");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "hidden";
+            hid[0].style.opacity = "0%";
+        }
+}
+function showcnn5(){
+        var hid = document.getElementsByClassName("cnn5");
+        // Emulates jQuery $(element).is(':hidden');
+        if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+            hid[0].style.visibility = "visible";
+            hid[0].style.opacity = "100%";
+        }
+}
+
+function showRelu1(){
+
+    var hid = document.getElementsByClassName("relu1");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "visible";
+        hid[0].style.opacity = "100%";
+    }
+    var hid = document.getElementsByClassName("relu2");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "hidden";
+        hid[0].style.opacity = "0%";
+    }
+}
+function showRelu2(){
+
+    var hid = document.getElementsByClassName("relu1");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "hidden";
+        hid[0].style.opacity = "0%";
+    }
+    var hid = document.getElementsByClassName("relu2");
+    // Emulates jQuery $(element).is(':hidden');
+    if(hid[0].offsetWidth > 0 && hid[0].offsetHeight > 0) {
+        hid[0].style.visibility = "visible";
+        hid[0].style.opacity = "100%";
+    }
+}
+
+
