439f6eb30e57f7640f646e9360fe4049e9ff4db5
JacquelineRJ <73772376+JacquelineRJ@users.noreply.github.com>
2023-12-07 15:06:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function showExamples(data) {
   const examples = data.nextTestBatch(20);
   const numExamples = examples.xs.shape[0];
   
-  // Create a canvas element to render each example
+  // Create a canvas 
   for (let i = 0; i < numExamples; i++) {
     const imageTensor = tf.tidy(() => {
       // Reshape the image to 28x28 px
@@ -48,12 +48,10 @@ function getModel() {
       kernelInitializer: 'varianceScaling'
     }));
   
-    // The MaxPooling layer acts as a sort of downsampling using max values
-    // in a region instead of averaging.  
+    // The MaxPooling layer s
     model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
     
     // Repeat another conv2d + maxPooling stack. 
-    // Note that we have more filters in the convolution.
     model.add(tf.layers.conv2d({
       kernelSize: 5,
       filters: 16,
@@ -63,13 +61,8 @@ function getModel() {
     }));
     model.add(tf.layers.maxPooling2d({poolSize: [2, 2], strides: [2, 2]}));
     
-    // Now we flatten the output from the 2D filters into a 1D vector to prepare
-    // it for input into our last layer. This is common practice when feeding
-    // higher dimensional data to a final classification output layer.
     model.add(tf.layers.flatten());
   
-    // Our last layer is a dense layer which has 10 output units, one for each
-    // output class (i.e. 0, 1, 2, 3, 4, 5, 6, 7, 8, 9).
     const NUM_OUTPUT_CLASSES = 10;
     model.add(tf.layers.dense({
       units: NUM_OUTPUT_CLASSES,
@@ -79,7 +72,6 @@ function getModel() {
   
     
     // Choose an optimizer, loss function and accuracy metric,
-    // then compile and return the model
     const optimizer = tf.train.adam();
     model.compile({
       optimizer: optimizer,
@@ -160,19 +152,6 @@ async function showConfusion(model, data) {
 }
 
 
-// run program
-// async function run() {  
-//     const data = new MnistData();
-//     await data.load();
-//     await showExamples(data);
-//     const model = getModel();
-//     tfvis.show.modelSummary({name: 'Model Architecture', tab: 'Model'}, model);
-    
-//   await train(model, data);
-//   await showAccuracy(model, data);
-//   await showConfusion(model, data);
-//   }
-
 // load Data program
 var data = new MnistData();
 async function loadData() {  
@@ -196,7 +175,6 @@ async function showConfMatrix(){
   await showConfusion(trainModel, data);
 }
 
-// document.addEventListener('DOMContentLoaded', run);
 
 //Train Button
 var visorButton = document.getElementById('show-visor-button');
